951ca263fabd81584d1cd520cee90af8b48e22b0
Renad95 <alfurayhi95@gmail.com>
2024-05-15 12:22:44 +0300

    added chat list and Notification

---
+++ LocalBuddy App 1/ViewModel/View/ChatlistView.swift
@@ -0,0 +1,18 @@
+//
+//  ChatlistView.swift
+//  LocalBuddy 
+//
+//  Created by Renad fahad Alfurayhi on 07/11/1445 AH.
+//
+
+import SwiftUI
+
+struct ChatlistView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    ChatlistView()
+}
---
+++ LocalBuddy App 1/ViewModel/View/NotificationView.swift
@@ -0,0 +1,18 @@
+//
+//  NotificationView.swift
+//  LocalBuddy 
+//
+//  Created by Renad fahad Alfurayhi on 07/11/1445 AH.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NotificationView()
+}
