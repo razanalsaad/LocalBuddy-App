137824d034889f2cf0df7a1e5b925dddcbdb2efd
Renad95 <alfurayhi95@gmail.com>
2024-05-15 12:34:33 +0300

    edit the color

--- LocalBuddy App 1/ViewModel/View/ChatlistView.swift
+++ LocalBuddy App 1/ViewModel/View/ChatlistView.swift
@@ -9,7 +9,89 @@ import SwiftUI
 
 struct ChatlistView: View {
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+        let text1Color = Color.mustard
+        let text2Color = Color.gray
+            VStack {
+                Text("Chat")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .padding(.top)
+                
+                ScrollView {
+                    VStack(alignment: .leading, spacing: 20) {
+                        HStack(spacing: 20) {
+                            // Yellow circle - Profile 1
+                            Circle()
+                                .fill(Color.mustard)
+                                .frame(width: 70, height: 100)
+                                .padding(.leading)
+                            
+                            // Message - Profile 1
+                            VStack(alignment: .leading) {
+                                Text("John Doe")
+                                    .font(.headline)
+                                    .foregroundColor(.black)
+                                    
+                                Text("You :")
+                                               .foregroundColor(text1Color)
+                                               .font(.body)
+                                           
+                                           Text("OK")
+                                               .foregroundColor(text2Color)
+                                               .font(.body)
+
+                                               .font(.body)
+                                               
+                            }
+                            
+                            Spacer()
+                            
+                            // Timestamp - Profile 1
+                            Text("10:30 AM")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                                .padding(.trailing)
+                        }
+                        
+                        Divider() // Gray line separating chat items
+                        HStack(spacing: 20) {
+                            // Yellow circle - Profile 2
+                            Circle()
+                                .fill(Color.mustard)
+                                .frame(width: 70, height: 100)
+                                .padding(.leading)
+                            
+                            // Message - Profile 2
+                            VStack(alignment: .leading) {
+                                Text("Jane Smith")
+                                    .font(.headline)
+                                    .foregroundColor(.black)
+                                Text("You :")
+                                               .foregroundColor(text1Color)
+                                               .font(.body)
+                                  
+                                Text("How are you?")
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+                            }
+                            
+                            Spacer()
+                            
+                            // Timestamp - Profile 2
+                            Text("12:45 PM")
+                                .font(.subheadline)
+                                .foregroundColor(.gray)
+                                .padding(.trailing)
+                        }
+                    }
+                    Divider() // Gray line separating chat items
+                    .padding()
+                }
+                Spacer()
+            }
+            .navigationBarTitleDisplayMode(.inline)
+        }
     }
 }
 
--- LocalBuddy App 1/ViewModel/View/NotificationView.swift
+++ LocalBuddy App 1/ViewModel/View/NotificationView.swift
@@ -4,12 +4,77 @@
 //
 //  Created by Renad fahad Alfurayhi on 07/11/1445 AH.
 //
-
 import SwiftUI
 
+struct Profile: Identifiable {
+    let id = UUID()
+    let profilePicture: Image
+    let username: String
+    let timestamp: String
+}
+
 struct NotificationView: View {
+    @State private var profiles: [Profile] = [
+        Profile(profilePicture: Image(systemName: "person.circle.fill")
+                , username: "John Doe", timestamp: "2 minutes ago"),
+        Profile(profilePicture: Image(systemName: "person.circle.fill"), username: "Jane Smith", timestamp: "5 minutes ago")
+    ]
+
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            ScrollView {
+                VStack(alignment: .leading, spacing: 16) {
+                    ForEach(profiles) { profile in
+                        HStack(alignment: .top, spacing: 40) {
+                            profile.profilePicture
+                                .font(.title)
+                                .frame(width: 50, height: 50)
+                                .foregroundColor(.mustard)
+                                .padding()
+                            VStack(alignment: .leading, spacing:4) {
+                                Text(profile.username)
+                                    .font(.headline)
+                                Text(profile.timestamp)
+                                    .font(.subheadline)
+                                    .foregroundColor(.gray)
+
+                                Spacer()
+
+                                HStack(spacing: 25) {
+                                    Button(action: {
+                                        // Action for the "Accept" button
+                                        
+                                    }) {
+                                        Text("Accept")
+                                            .foregroundColor(.white)
+                                            .padding()
+                                            .background(Color.black)
+                                            .cornerRadius(10)
+                                    }
+
+                                    Button(action: {
+                                        // Action for the "Reject" button
+                                        
+                                    }) {
+                                        Text("Reject")
+                                            .foregroundColor(.black)
+                                            .padding()
+                                            .background(Color.gray2)
+                                            .cornerRadius(10)
+                                    }
+                                }
+                            }
+                        }
+                        Divider()
+                        .padding()
+                        .background(Color.white)
+                        .cornerRadius(10)
+                    }
+                }
+                .padding()
+            }
+            .navigationBarTitle("Notification")
+        }
     }
 }
 
